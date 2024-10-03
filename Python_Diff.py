024a3593625f374b2277ccb27ab56b8ceeea89af
Kappa56799 <palka@kacper.boo>
2024-10-03 09:20:09 +0100

    Fully working diff just like the command

--- Python_Diff.py
+++ Python_Diff.py
@@ -10,46 +10,62 @@ def main():
 
 def diff(files):
     with open(files[0], 'r') as f1:
-        with open(files[1], 'r') as f2:
-            file1_lines = f1.readlines()
-            file2_lines = f2.readlines()
+        file1_lines = f1.readlines()
+    with open(files[1], 'r') as f2:
+        file2_lines = f2.readlines()
 
     file1_length = len(file1_lines)
     file2_length = len(file2_lines)
 
     i, j = 0, 0
 
+    file1_buffer = []
+    file2_buffer = []
+    change_start_i = change_start_j = None
+
     while i < file1_length or j < file2_length:
-        if i >= file1_length:
-            print(f"{i}a{j+1},{file2_length}")
-            for k in range(j, file2_length):
-                print(f"> {file2_lines[k].strip()}")
-            break
-        elif j >= file2_length:
-            print(f"{i+1},{file1_length}d{j}")
-            for k in range(i, file1_length):
-                print(f"< {file1_lines[k].strip()}")
-            break
-        elif file1_lines[i] != file2_lines[j]:
-            if file1_lines[i:i+1] != file2_lines[j:j+1]:
-                if file1_lines[i+1:i+2] == file2_lines[j:j+1]:
-                    print(f"{i+1}d{j+1}")
-                    print(f"< {file1_lines[i].strip()}")
-                    i += 1
-                elif file1_lines[i:i+1] == file2_lines[j+1:j+2]:
-                    print(f"{i}a{j+1}")
-                    print(f"> {file2_lines[j].strip()}")
-                    j += 1
-                else:
-                    print(f"{i+1}c{j+1}")
-                    print(f"< {file1_lines[i].strip()}")
-                    print("---")
-                    print(f"> {file2_lines[j].strip()}")
-                    i += 1
-                    j += 1
-        else:
+        if i < file1_length and j < file2_length:
+            if file1_lines[i] != file2_lines[j]:
+                if change_start_i is None:
+                    change_start_i = i
+                    change_start_j = j
+                file1_buffer.append(f"< {file1_lines[i]}")
+                file2_buffer.append(f"> {file2_lines[j]}")
+            else:
+                if file1_buffer or file2_buffer:
+                    print_changes(change_start_i, i, change_start_j, j, file1_buffer, file2_buffer)
+                    file1_buffer = []
+                    file2_buffer = []
+                    change_start_i = change_start_j = None
             i += 1
             j += 1
+        elif i < file1_length:
+            if change_start_i is None:
+                change_start_i = i
+                change_start_j = j
+            file1_buffer.append(f"< {file1_lines[i]}")
+            file2_buffer.append("> ")
+            i += 1
+        elif j < file2_length:
+            if change_start_i is None:
+                change_start_i = i
+                change_start_j = j
+            file1_buffer.append("< ")
+            file2_buffer.append(f"> {file2_lines[j]}")
+            j += 1
+
+    if file1_buffer or file2_buffer:
+        print_changes(change_start_i, i, change_start_j, j, file1_buffer, file2_buffer)
+
+def print_changes(start_i, end_i, start_j, end_j, file1_buffer, file2_buffer):
+    if end_i - start_i == 1 and end_j - start_j == 1:
+        print(f"{start_i+1}c{start_j+1}")
+    else:
+        print(f"{start_i+1},{end_i}c{start_j+1},{end_j}")
+    
+    print("".join(file1_buffer), end="")
+    print("---")
+    print("".join(file2_buffer), end="")
 
 if __name__ == '__main__':
     main()
